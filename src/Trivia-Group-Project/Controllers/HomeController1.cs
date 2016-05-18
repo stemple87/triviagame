3a785a48a283aa83d65e6500b67775e0f74cc9d0
Jay, Matt, Simon, Chris <mnewell10@gmail.com>
2016-05-18 09:53:38 -0700

    Add fail modal

--- src/Trivia-Group-Project/Controllers/HomeController1.cs
+++ src/Trivia-Group-Project/Controllers/HomeController1.cs
@@ -53,6 +53,10 @@ namespace Trivia_Group_Project.Controllers
         {
             return View();
         }
+        public IActionResult WrongModal()
+        {
+            return View();
+        }
 
         ////Post /Game /Alternate
         //[HttpPost]
